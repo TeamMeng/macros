878c10874e05e8d145f52ba3c5661bfa5ad4f8c3
TeamMeng <862915265@qq.com>
2024-09-09 20:16:20 +0800

    feature: use darling to process EnumFrom macro

---
+++ examples/enum_macro_darling.rs
@@ -0,0 +1,34 @@
+use macros::EnumFromDarling;
+
+#[allow(unused)]
+#[derive(EnumFromDarling, Debug)]
+enum Direction<T> {
+    Up(DirectionUp<T>),
+    Down,
+    Left(u32),
+    Right,
+}
+
+#[allow(unused)]
+#[derive(Debug)]
+struct DirectionUp<T> {
+    speed: T,
+}
+
+fn main() {
+    let up: Direction<i32> = DirectionUp::new(42).into();
+    let left: Direction<i32> = 10.into();
+    println!("{:?}, {:?}", up, left);
+}
+
+impl<T> DirectionUp<T> {
+    fn new(speed: T) -> Self {
+        Self { speed }
+    }
+}
+
+impl<T> From<i32> for Direction<T> {
+    fn from(value: i32) -> Self {
+        Direction::Left(value as u32)
+    }
+}
